c9e1b7ab3847c05cf3198c11ae41be7971d69d98
MaeveWheaton <teamheaven10@outlook.com>
2021-06-03 13:16:56 -0400

    coded

--- CardDeck/Form1.cs
+++ CardDeck/Form1.cs
@@ -15,14 +15,24 @@ namespace CardDeck
         //standard deck of cards
         List<string> deck = new List<string>();
 
+        //hands
+        List<string> dealerCards = new List<string>();
+        List<string> playerCards = new List<string>();
+
+        Random randGen = new Random();
+        int card;
 
         public Form1()
         {
             InitializeComponent();
+            SetDeck();            
+        }
 
+        public void SetDeck()
+        {
             //fill deck with standard 52 cards
             //H - Hearts, D - Diamonds, C - Clubs, S - Spades
-            
+
             deck.AddRange(new string[] { "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH", "AH" });
             deck.AddRange(new string[] { "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD", "AD" });
             deck.AddRange(new string[] { "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC", "AC" });
@@ -32,16 +42,53 @@ namespace CardDeck
         private void shuffleButton_Click(object sender, EventArgs e)
         {
             //re-arrange the order of the cards in the deck list
+            List<string> shuffledDeck = new List<string>();
+
+            while (deck.Count() > 0)
+            {
+                card = randGen.Next(0, deck.Count());
+
+                shuffledDeck.Add(deck[card]);
+                deck.Remove(deck[card]);
+            }
+
+            deck = shuffledDeck;
         }
 
         private void dealButton_Click(object sender, EventArgs e)
         {
             //deal 5 cards each to dealer and player and display them
+
+            for (int i = 0; i < 5; i++)
+            {
+                playerCards.Add(deck[0]);
+                deck.Remove(deck[0]);
+                
+                dealerCards.Add(deck[0]);
+                deck.Remove(deck[0]);
+            }
+
+            dealerCardsLabel.Text = "";
+            playerCardsLabel.Text = "";
+
+            for (int i = 0; i < playerCards.Count; i++)
+            {
+                dealerCardsLabel.Text += $"{dealerCards[i]} ";
+                playerCardsLabel.Text += $"{playerCards[i]} ";
+            }
         }
 
         private void collectButton_Click(object sender, EventArgs e)
         {
             //put player and dealer cards back into the deck
+            deck.AddRange(playerCards);
+            deck.AddRange(dealerCards);
+
+            playerCards.Clear();
+            dealerCards.Clear();
+
+            dealerCardsLabel.Text = "";
+            playerCardsLabel.Text = "";
         }
     }
 }
